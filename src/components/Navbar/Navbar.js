4aa71cea4bb917842a7d7f589523f52091c7c4e4
Christine Lee <chriistinelee@gmail.com>
2020-09-09 17:18:58 -0700

    implement navigation bar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Navbar.scss';
+
+class Navbar extends React.Component {
+  render() {
+    const {
+      brand,
+      menu,
+      end
+    } = this.props;
+
+    return (
+      <div className="navbar-container">
+        <div className="navbar-start">
+          <div className="navbar-brand">
+            { brand }
+          </div>
+          <div className="navbar-menu">
+            { menu }
+          </div>
+        </div>
+        <div className="navbar-end">
+          { end }
+        </div>
+      </div>
+    );
+  }
+}
+export default Navbar;
