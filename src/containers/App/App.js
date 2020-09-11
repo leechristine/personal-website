3cce3422a8a9a7fe32ae0e1efd2199fddd13a3d2
Christine Lee <chriistinelee@gmail.com>
2020-09-11 01:33:34 -0700

    add landing page to app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
+
+import Navbar from 'components/Navbar/Navbar';
+import ToggleButton from 'components/Button/ToggleButton';
+import Landing from 'containers/Landing/Landing';
 import './App.scss';
 
 
 class App extends React.Component {
+
+  state = {theme: 'light'}
+
+  toggleTheme = () => {
+    if (this.state.theme === 'light') {
+      this.setState({theme: 'dark'});
+    }
+    else {
+      this.setState({theme: 'light'});
+    }
+  }
+
   render() {
     return (
-      <div className="app">
+      <div className={ `app theme-${ this.state.theme }` }>
+        <Navbar
+          brand={
+            <a className="navbar-item" href="#">home</a>
+          }
+          menu={
+            <React.Fragment>
+              <a className="navbar-item" href="#">about me</a>
+              <a className="navbar-item" href="#">coding</a>
+            </React.Fragment>
+          }
+          end={
+            <ToggleButton type="mode" color="secondary" onToggle={ this.toggleTheme } />
+          }
+        />
+        <Landing />
       </div>
     );
   }
